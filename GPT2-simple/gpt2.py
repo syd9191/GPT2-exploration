5e01e85825d5bd95125187df34832e7c4f2ff49b
syd9191 <sydney9191@gmail.com>
2025-06-26 10:37:46 +0800

    Finetuning first stage done

--- GPT2-simple/gpt2.py
+++ GPT2-simple/gpt2.py
@@ -17,137 +17,164 @@ from model.model_tracker import ModelTracker
 
 # ---------------- sampling loop, we could probably abstract this later on 
 if __name__=="__main__":
-    data_path     = "./GPT2-exploration/data/openwebtext"
+    data_path     = "./GPT2-exploration/data/smol_smoltalk"
     model_name    = "gpt-2"
     B, T          = 16, 1024 #micro batch size
-    max_lr        = 2e-4
-    max_steps     = 7066
-    save_interval = 300
+    max_lr        = 6e-4
+    max_steps     = 100
+    save_interval = 25
     batch_size    = 524288 #2**19 we stick with the pow 2 again
     enc           = tiktoken.get_encoding("gpt2")
-    prompt        = "I am a large language model,"
+    prompt        = "Describe the colour yellow?</s>"
+    mode          = "eval"
 
     assert batch_size%(B*T)==0 
 
     grad_accum_steps=batch_size//(B*T)
 
-    plot_path   = f"./GPT2-exploration/analytics/{model_name}/plots/{model_name}_loss_curve.png"
+    plot_path   = f"./GPT2-exploration/analytics/{model_name}/plots/{model_name}_finetuning_loss_curve.png"
     model_path  = f"./GPT2-exploration/models/{model_name}/{model_name}.pth"
     stats_path  = f"./GPT2-exploration/analytics/{model_name}/stats/{model_name}_training_stats.json"
-    loss_path   = f"./GPT2-exploration/analytics/{model_name}/stats/{model_name}.npy"
-
-    dir_path=Path(data_path)
+    loss_path   = f"./GPT2-exploration/analytics/{model_name}/stats/{model_name}_finetuning.npy"
 
     device, use_amp_cuda = utils.get_device(verbose=True)
 
-    dataset = DirStreamingDataset(path=data_path, 
-                                  block_size=T,
-                                  seed=42)
-    val_dataset = DirStreamingDataset(path=data_path, 
-                                      block_size=T,
-                                      seed=24) #another instance for val use
+    if mode=="train":
+        dir_path=Path(data_path)
 
-    torch.set_float32_matmul_precision('high')
+        
 
-    n_tokens= dataset.get_dataset_token_count()
-    epoch_size= int(n_tokens/(B*T))
-    print(f"epoch_size: {epoch_size}")
+        dataset = DirStreamingDataset(path=data_path, 
+                                    block_size=T,
+                                    seed=42)
+        val_dataset = DirStreamingDataset(path=data_path, 
+                                        block_size=T,
+                                        seed=24) #another instance for val use
 
-    if torch.cuda.device_count() > 1:
-        torch.distributed.init_process_group(backend="nccl", init_method="env://")
-        local_rank = int(os.environ.get("LOCAL_RANK", 0))
-        torch.cuda.set_device(local_rank)
-        sampler = DistributedSampler(dataset, shuffle=False)
-        val_sampler= DistributedSampler(val_dataset, shuffle=False)
-        ddp=True
-    else:
-        sampler = None
-        ddp=False
+        torch.set_float32_matmul_precision('high')
 
-    loader = utils.make_loader(
-        dataset,
-        batch_size=B,
-        num_workers=4,            # no separate workers ➜ no pickling
-        sampler=sampler,
-        device_is_cuda=device.type == "cuda",
-    )
+        n_tokens= dataset.get_dataset_token_count()
+        epoch_size= int(n_tokens/(B*T))
+        print(f"epoch_size: {epoch_size}")
 
-    val_loader = utils.make_loader(
-        val_dataset,
-        batch_size=B,
-        num_workers=4,            # no separate workers ➜ no pickling
-        sampler=val_sampler,
-        device_is_cuda=device.type == "cuda",
-    )
+        if torch.cuda.device_count() > 1:
+            torch.distributed.init_process_group(backend="nccl", init_method="env://")
+            local_rank = int(os.environ.get("LOCAL_RANK", 0))
+            torch.cuda.set_device(local_rank)
+            sampler = DistributedSampler(dataset, shuffle=False)
+            val_sampler= DistributedSampler(val_dataset, shuffle=False)
+            ddp=True
+        else:
+            sampler = None
+            ddp=False
 
-    utils.smoke_test(B=B,
-                    T=T,
-                    loader=loader) #test before training
+        loader = utils.make_loader(
+            dataset,
+            batch_size=B,
+            num_workers=4,            # no separate workers ➜ no pickling
+            sampler=sampler,
+            device_is_cuda=device.type == "cuda",
+        )
 
-    model = GPT(config=GPTConfig(),
-                sampler=sampler,
-                enc=enc).to(device)
-    model = torch.compile(model)
+        val_loader = utils.make_loader(
+            val_dataset,
+            batch_size=B,
+            num_workers=4,            # no separate workers ➜ no pickling
+            sampler=val_sampler,
+            device_is_cuda=device.type == "cuda",
+        )
 
-    optimiser = model.configure_optimizers(weight_decay=0.1,
-                                           learning_rate=max_lr
-                                           )
-    
+        utils.smoke_test(B=B,
+                        T=T,
+                        loader=loader) #test before training
 
-    try:
-        print(f"Loading weights from: {model_path}")
-        model.load_weights(path=model_path,
-                        device=device,
-                        weights_only=True)
-    except Exception as e:
-        print(f"No pre-trained weights found at {model_path}. Starting from scratch.")
-        print(f"Error: {e}")
-    
+        model = GPT(config=GPTConfig(),
+                    sampler=sampler,
+                    enc=enc).to(device)
+        model = torch.compile(model)
 
-    if torch.cuda.device_count() > 1:
-        model = nn.parallel.DistributedDataParallel(
-            model, device_ids=[torch.cuda.current_device()], output_device=torch.cuda.current_device()
-        ) # sync gradients
+        optimiser = model.configure_optimizers(weight_decay=0.1,
+                                            learning_rate=max_lr
+                                            )
+        
 
-    scaler_device   = "cuda" if device.type == "cuda" else "cpu"
-    use_device_amp  = device.type == "cuda"               # enable only on CUDA
-    scaler          = torch.amp.GradScaler(scaler_device,
-                                     enabled=use_device_amp)
+        try:
+            print(f"Loading weights from: {model_path}")
+            model.load_weights(path=model_path,
+                            device=device,
+                            weights_only=True)
+        except Exception as e:
+            print(f"No pre-trained weights found at {model_path}. Starting from scratch.")
+            print(f"Error: {e}")
+        
 
-    model.train()
+        if torch.cuda.device_count() > 1:
+            model = nn.parallel.DistributedDataParallel(
+                model, device_ids=[torch.cuda.current_device()], output_device=torch.cuda.current_device()
+            ) # sync gradients
 
-    model_tracker=ModelTracker(model=model,
-                            batch_size=B,
-                            seq_length=T,
-                            total_tokens=n_tokens,
-                            plot_path=plot_path,
-                            model_path=model_path,
-                            stats_path=stats_path,
-                            loss_path=loss_path )
-    
+        scaler_device   = "cuda" if device.type == "cuda" else "cpu"
+        use_device_amp  = device.type == "cuda"               # enable only on CUDA
+        scaler          = torch.amp.GradScaler(scaler_device,
+                                        enabled=use_device_amp)
 
-    model.module.training_loop(max_steps=max_steps,
-                  save_interval=save_interval,
-                  batch_size=B, 
-                  seq_len=T, 
-                  max_lr=max_lr, 
-                  test_prompt=prompt,
-                  grad_accum_steps=grad_accum_steps,
-                  use_amp_cuda=use_amp_cuda,
-                  ddp=ddp, 
-                  loader=loader, 
-                  val_loader=val_loader, 
-                  model_tracker=model_tracker,
-                  device=device.type,
-                  scaler=scaler,
-                  optimiser=optimiser)
+        model.train()
 
-    model.module.invoke(prompt=prompt,
-                 max_new_tokens=30, 
-                 device=device, 
-                 )
+        model_tracker=ModelTracker(model=model,
+                                batch_size=B,
+                                seq_length=T,
+                                total_tokens=n_tokens,
+                                plot_path=plot_path,
+                                model_path=model_path,
+                                stats_path=stats_path,
+                                loss_path=loss_path )
+        
 
-    import  sys;sys.exit(0)
+        model.module.training_loop(max_steps=max_steps,
+                    save_interval=save_interval,
+                    batch_size=B, 
+                    seq_len=T, 
+                    max_lr=max_lr, 
+                    test_prompt=prompt,
+                    grad_accum_steps=grad_accum_steps,
+                    use_amp_cuda=use_amp_cuda,
+                    ddp=ddp, 
+                    loader=loader, 
+                    val_loader=val_loader, 
+                    model_tracker=model_tracker,
+                    device=device.type,
+                    scaler=scaler,
+                    optimiser=optimiser)
+
+        model.module.invoke(prompt=prompt,
+                    max_new_tokens=30, 
+                    device=device, 
+                    )
+
+        import  sys;sys.exit(0)
+
+    elif mode=="eval":
+        model = GPT(config=GPTConfig(),
+                    sampler=None,
+                    enc=enc).to(device)
+        model = torch.compile(model)
+
+        try:
+            print(f"Loading weights from: {model_path}")
+            model.load_weights(path=model_path,
+                            device=device,
+                            weights_only=True)
+        except Exception as e:
+            print(f"No pre-trained weights found at {model_path}. Starting from scratch.")
+            print(f"Error: {e}")
+
+        response=model.invoke(prompt=prompt,
+                    max_new_tokens=300, 
+                    device=device, 
+                    )
+        
+        print(response)
+        
 
     """
     #use this code if we wanna hack generation loop
